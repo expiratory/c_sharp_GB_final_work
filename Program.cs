ad1fa443d41e6ef6e29756db6e745b76c7cf759b
Aleksandr_Kilchevskiy <alexkill94@mail.ru>
2022-11-30 20:16:08 +0300

    add program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿Console.Clear();
+
+void PrintStringWithOnlyThreeSymbols(string[] stringArray)
+{
+    int count = 0;
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= 3) count++;
+    }
+    string[] newStringArray = new string [count];
+    int index = 0;
+    for (int g = 0; g < stringArray.Length; g++)
+    {
+            if (stringArray[g].Length <= 3)
+            {
+                newStringArray[index] = stringArray[g];
+                index++;
+            }
+    }
+    Console.WriteLine($"[ {String.Join(", ", newStringArray)} ]");
+}
+
+string[] initialStringArray = {"hello", "2", "world", ":-)"};
+Console.WriteLine($"[ {String.Join(", ", initialStringArray)} ]");
+Console.WriteLine();
+PrintStringWithOnlyThreeSymbols(initialStringArray);
